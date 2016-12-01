b310fe494e10f0ac6cd2a78d9dc479487e6ae8ba
王成 <274281610@qq.com>
2016-12-01 16:36:10 +0800

    11

---
+++ AwesomeProject/personal_info.js
@@ -0,0 +1,29 @@
+/**
+ * Created by wangc on 2016/12/1.
+ * 个人中心
+ */
+import React, {Component} from 'react';
+import {
+    AppRegistry, Text,
+    StyleSheet,
+    Image,
+    View,
+    TextInput,
+    TouchableHighlight,
+    Navigator,
+    console,
+    Alert,
+    ToastAndroid,
+} from 'react-native';
+
+export  default  class  PersonalInfo extends Component{
+    render(){
+        return(
+            <View>
+
+
+            </View>
+        );
+    }
+
+}
